f2bdb07eb4d3cbbb374021ab0843a2648906c1b4
AHchaker <abdulhamidchaker@gmail.com>
2023-03-08 17:00:57 -0600

    completed assignment

---
+++ 9.10.py
@@ -0,0 +1,15 @@
+#hamid chaker 2060843
+import csv
+csv_input = input()
+
+with open(csv_input, 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        list_of_words = row
+
+
+no_duplicates = list(dict.fromkeys(words_list))
+listlength = len(no_duplicates)
+
+for i in range(listlength):
+    print(no_duplicates[i], words_list.count(no_duplicates[i]))
