8aab06c833c2040d5dbc042df62ad4b7283f3b39
AHChaker <abdulhamidchaker@gmail.com>
2023-03-26 16:30:12 -0500

    Completed assignment with full grade

--- 10.15.py
+++ 10.15.py
@@ -1,29 +1,25 @@
+#Hamid Chaker 2060843
 class Team:
     def __init__(self):
-        self.teamname = 'none'
-        self.teamwins = 0
+        self.team_name = 'none'
+        self.team_wins = 0
         self.team_losses = 0
 
     def get_win_percentage(self):
-        team_score = self.teamwins / (self.teamwins + self.team_losses)
-        return team_score
-
+        return self.team_wins / (self.team_wins + self.team_losses)
 
 if __name__ == "__main__":
-
     student_team = Team()
 
     teamname = input()
     teamwins = int(input())
     team_losses = int(input())
 
-    student_team.teamname = teamname
-    student_team.teamwins = teamwins
+    student_team.team_name = teamname
+    student_team.team_wins = teamwins
     student_team.team_losses = team_losses
 
-    win_percent = student_team.get_win_percentage()
-
-    if win_percent < 0.5:
-        print(f'Team {student_team.teamname} has a losing average.')
-    elif win_percent >= 0.5:
-        print(f"Congratulations, Team {student_team.teamname} has a winning average!")
+    if student_team.get_win_percentage() < 0.5:
+        print(f'Team {student_team.team_name} has a losing average.')
+    else:
+        print(f"Congratulations, Team {student_team.team_name} has a winning average!")
